6eb604d52221b4ecb7bc86de006af049ed0cf37b
Alexander V <avjslearn@gmail.com>
2019-04-06 16:41:15 +0700

    Solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,31 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.buf = '';
   }
   
+  pushLine(callback, flush) {
+    if (this.buf.indexOf(os.EOL) !== -1) {
+      let lines = this.buf.split(os.EOL);
+      let line = lines.splice(0, 1)[0];
+      this.buf = lines.join(os.EOL);
+
+      callback(null, line);
+    } else if (flush) {
+      callback(null, this.buf);
+    } else {
+      callback(null);
+    }
+  }
+
   _transform(chunk, encoding, callback) {
+    this.buf += chunk.toString();
+    this.pushLine(callback);    
   }
   
   _flush(callback) {
+    this.pushLine(callback, true);
   }
 }
 
